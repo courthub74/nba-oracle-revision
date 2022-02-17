21448fa5f3257f4da3a12f6f24f0df3ee44635be
Courdevelops <courdevelops@gmail.com>
2022-02-17 13:50:54 -0500

    apisuccess

--- nbaapp/urls.py
+++ nbaapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('atlanta/', views.atl, name='atl'),
+    path('boston/', views.bos, name='bos'),
 ]
--- nbaapp/views.py
+++ nbaapp/views.py
@@ -1,4 +1,7 @@
+from urllib import request
 from django.shortcuts import render
+import requests
+import json
 
 #HOME
 def home(request):
@@ -6,14 +9,33 @@ def home(request):
 
 ###################################################################################
 
-#TEAMS
+#TEAMS (API Request)
 
 #ATLANTA HAWKS
-def hawks(request):
+def atl(request):
+    import requests
+    import json
+
+    # ATLANTA General Info 134880
+    atlreq = requests.get("https://www.thesportsdb.com/api/v1/json/4013017/searchteams.php?t=Atlanta%20Hawks")
+    atl_info = json.loads(atlreq.content)
+
+    return render(request, "teams/atlanta.html", {'atl_info': atl_info})
+
+
+#BOSTON CELTICS
+def bos(request):
     import requests
     import json 
 
-# ATLANTA General Info 134880
+    #BOSTON General Info 134860
+    bosreq = requests.get("https://www.thesportsdb.com/api/v1/json/4013017/searchteams.php?t=Boston%20Celtics")
+    bos_info = json.loads(bosreq.content)
+
+    return render(request, "teams/boston.html", {'bos_info': bos_info})
+
+
+
 
 
 

7329acce7494c1066bc4691597bffbd627ed03b4
Courdevelops <courdevelops@gmail.com>
2022-02-16 19:15:32 -0500

    teamsinviews

--- nbaapp/views.py
+++ nbaapp/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 #HOME
 def home(request):
     return render(request, "home.html", {})
+
+###################################################################################
+
+#TEAMS
+
+#ATLANTA HAWKS
+def hawks(request):
+    import requests
+    import json 
+
+# ATLANTA General Info 134880
+
+
+
+
+
